8ebcf27a04d5c6fda9f395898d9f52f973fac041
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2025-03-11 09:33:56 -0300

    feat: create sidebar component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,16 @@
 import styles from "./page.module.css";
 import Header from "../components/header";
+import Sidebar from "../components/sidebar";
 
 export default function Home() {
   return (
     <div>
       <Header />
+
+      <main className={styles.dashboardContainer}>
+        <Sidebar />
+      </main>
+
     </div>
   );
 }
