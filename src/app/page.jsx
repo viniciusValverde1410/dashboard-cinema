c4a95d404514b3119736d904c54c206286e4ea2d
Vinicius Valverde <vinicius.valverde@aluno.senai.br>
2025-03-11 09:11:27 -0300

    feat: create header component

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
 import styles from "./page.module.css";
+import Header from "../components/header";
 
 export default function Home() {
   return (
     <div>
-      <h1 className={styles.title}>Hello World</h1>
+      <Header />
     </div>
   );
 }
